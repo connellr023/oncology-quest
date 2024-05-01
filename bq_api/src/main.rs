10a63f3463e019e76cf1531dbdca4cbdbd9c222c
connellr023 <connell.reffo@ucalgary.ca>
2024-05-01 11:51:50 -0600

    server

--- bq_api/src/main.rs
+++ bq_api/src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+mod routes;
+mod models;
+
+use routes::*;
+use models::environment::Environment;
+use std::io;
+use dotenv::dotenv;
+use actix_web::{web, App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> io::Result<()> {
+    // Load environment variables
+    dotenv().ok();
+    let env = Environment::new().expect("Failed to read environment variables");
+
+    // Start HTTP server
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index::index))
+    })
+    .bind(format!("{}:{}", env.host_ip(), env.host_port()))?
+    .run()
+    .await
 }
