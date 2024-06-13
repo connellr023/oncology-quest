5c30b942cbb39b3e88cd0e1535ca1d5afb61e9d4
connellr023 <connell.reffo@ucalgary.ca>
2024-06-13 13:36:52 -0600

    Some minor stuff

--- oncology-quest-web/src/utilities.ts
+++ oncology-quest-web/src/utilities.ts
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = import.meta.env.DEV ? "http://127.0.0.1:8080" : "..."
+export const API_ENDPOINT = import.meta.env.DEV ? "http://127.0.0.1:8080" : ""
 
 export const USERNAME_REGEX = /^[a-zA-Z0-9\-\_\.]{1,25}$/
 export const NAME_REGEX = /^[a-zA-Z\s]{1,35}$/
