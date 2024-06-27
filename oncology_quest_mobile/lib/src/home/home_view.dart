7b00571e40a605fbaf33be6090a0e85deffe75aa
connellr023 <connell.reffo@ucalgary.ca>
2024-06-27 17:14:32 -0600

    Login/register screen

--- oncology_quest_mobile/lib/src/home/home_view.dart
+++ oncology_quest_mobile/lib/src/home/home_view.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import '../widgets/credit_footer.dart';
+import '../widgets/thematic_elevated_button.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    double buttonWidth = MediaQuery.of(context).size.width * 0.6;
+    const double buttonSpacing = 15;
+
     return Scaffold(
       body: Center(
         child: Column(
@@ -22,30 +26,52 @@ class HomeView extends StatelessWidget {
             const SizedBox(height: 16),
             RichText(
               text: TextSpan(
-                text: 'Get started with ',
+                text: 'Welcome to ',
                 style: TextStyle(
                   fontSize: MediaQuery.of(context).size.width * 0.045,
                   color: Theme.of(context).textTheme.bodySmall!.color,
                 ),
                 children: const <TextSpan>[
-                  TextSpan(text: 'Oncology Quest', style: TextStyle(fontWeight: FontWeight.bold)),
-                  TextSpan(text: ' below.'),
+                  TextSpan(text: 'Oncology Quest', style: TextStyle(fontWeight: FontWeight.bold))
                 ],
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor,
-                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30),
-                ),
-              ),
-              child: const Text('Login'),
+            const SizedBox(height: 6),
+            Text(
+              'an aid for Medical Oncology Trainees',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.width * 0.027,
+                color: Theme.of(context).textTheme.bodySmall!.color,
+              )
             ),
-          ],
+            const SizedBox(height: 25),
+            SizedBox(
+              width: buttonWidth,
+              height: 45,
+              child: ThematicElevatedButton(
+                text: 'Login',
+                onPressed: () => {},
+              ),
+            ),
+            const SizedBox(height: buttonSpacing),
+            SizedBox(
+              width: buttonWidth,
+              height: 45,
+              child: ThematicElevatedButton(
+                text: 'Register',
+                onPressed: () => {},
+              ),
+            ),
+            const SizedBox(height: buttonSpacing),
+            SizedBox(
+              width: buttonWidth,
+              height: 45,
+              child: ThematicElevatedButton(
+                text: 'Reset Password',
+                onPressed: () => {},
+              ),
+            )
+          ]
         )
       ),
       bottomNavigationBar: const Footer(),
