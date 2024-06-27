6de5f4aa86a2a3b89ea048d4a2b5d91f50a0c7df
connellr023 <connell.reffo@ucalgary.ca>
2024-06-27 12:45:11 -0600

    Work on home view

--- oncology_quest_mobile/lib/src/home/home_view.dart
+++ oncology_quest_mobile/lib/src/home/home_view.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
+import '../widgets/credit_footer.dart';
+
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
@@ -13,18 +15,31 @@ class HomeView extends StatelessWidget {
           children: <Widget>[
             SvgPicture.asset(
               'assets/images/logo.svg',
-              width: 100,
-              height: 100,
+              width: 130,
+              height: 130,
               color: Theme.of(context).primaryColor,
-
             ),
             const SizedBox(height: 16),
-            const Text(
-              'Get started with Oncology Quest below.'
+            // const Text(
+            //   'Get started with Oncology Quest below.'
+            // ),
+            RichText(
+              text: TextSpan(
+                text: 'Get started with ',
+                style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.045,
+                  color: Theme.of(context).textTheme.bodySmall!.color,
+                ),
+                children: const <TextSpan>[
+                  TextSpan(text: 'Oncology Quest', style: TextStyle(fontWeight: FontWeight.bold)),
+                  TextSpan(text: ' below.'),
+                ],
+              ),
             ),
           ],
         )
       ),
+      bottomNavigationBar: const Footer(),
     );
   }
 }
